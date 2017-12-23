bfc0d9d24216a0ac508db09b2cc426e36aabd628
Matt.Lyons <matt.lyons@envisionthen.com>
2017-12-22 21:24:48 -0500

    prep grunt

---
+++ gruntfile.js
@@ -0,0 +1,55 @@
+module.exports = function (grunt) {
+    "use strict";
+
+    grunt.initConfig({
+        copy: {
+            build: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: "./public",
+                        src: ["**"],
+                        dest: "./dist/public"
+                    },
+                    {
+                        expand: true,
+                        cwd: "./src",
+                        src: ["server.js"],
+                        dest: "./dist"
+                    }
+                ]
+            }
+        },
+        ts: {
+            app: {
+                files: [{
+                    src: ["src/\*\*/\*.ts", "!src/.baseDir.ts"],
+                    dest: "./dist"
+                }],
+                options: {
+                    module: "commonjs",
+                    target: "es6",
+                    experimentalDecorators: true,
+                    sourceMap: false,
+                    rootDir: "src"
+                }
+            }
+        },
+        watch: {
+            ts: {
+                files: ["src/\*\*/\*.ts"],
+                tasks: ["ts"]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-ts");
+
+    grunt.registerTask("default", [
+        "copy",
+        "ts"
+    ]);
+
+};
